fa0cf2796870fbdc8c7b7544b1d297ef65addad4
Igor Faria <igorfaria6@gmail.com>
2016-01-26 18:35:34 -0200

    Commit Teste

--- config/app.php
+++ config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'url' => 'http://laravel.dev',
+    'url' => 'http://localhost/laravel/curso-dev/public/',
 
     /*
     |--------------------------------------------------------------------------
